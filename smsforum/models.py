0b5e5a1c5a9aadebf646e7b66528e060efbf9af9
Jeff Wishnie <jeff@wishnie.org>
2009-06-25 09:00:17 +0000

    rewrite of best match to make _much_ simpler

--- smsforum/models.py
+++ smsforum/models.py
@@ -6,21 +6,46 @@ from apps.locations.models import Location
 from apps.nodegraph.models import NodeSet
 
 
-class Community(NodeSet):
-    name = models.CharField(max_length=255,unique=True, blank=False)
-    pass
-
-    def __unicode__(self):
-        return unicode(self.name)
-
-
 class Village(NodeSet):
+    # security masks
+    __SEC_MEMBER_ONLY_SEND=0x01
+    __SEC_MODERATED=0x02
+    __SEC_UNUSED_1=0x04
+    __SEC_UNUSED_2=0x08 
+
     name = models.CharField(max_length=255,unique=True, blank=False)
     location = models.ForeignKey(Location, null=True, blank=True)
-    pass
+
+    # Security flags recorded, but not yet enforced
+    _security =  models.PositiveSmallIntegerField(default=__SEC_MEMBER_ONLY_SEND)
+
+    # some useful props
+    def __get_sec_member_only_send(self):
+        return bool(self._security & self.__SEC_MEMBER_ONLY_SEND)
+
+    def __set_sec_member_only_send(self,val):
+        if bool(val):
+            self._security|=self.__SEC_MEMBER_ONLY_SEND
+        else:
+            self._security&=~self.__SEC_MEMBER_ONLY_SEND
+    sec_member_only_send=property(__get_sec_member_only_send,__set_sec_member_only_send)
+
+    def __get_sec_moderated(self):
+        return bool(self._security & self.__SEC_MODERATED)
+
+    def __set_sec_moderated(self,val):
+        if bool(val):
+            self._security|=self.__SEC_MODERATED
+        else:
+            self._security&=~self.__SEC_MODERATED
+    sec_moderated=property(__get_sec_moderated,__set_sec_moderated)
 
     def __unicode__(self):
         return unicode(self.name)
+
+
+class Community(Village):
+    pass
 
 #
 # 'Statics' as module level
