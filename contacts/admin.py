57030d650b4e17aa76c43eaeee53bb20f693be8b
Jeff Wishnie <jeff@wishnie.org>
2009-06-26 17:02:01 +0000

    some useful fields in admin app

--- contacts/admin.py
+++ contacts/admin.py
@@ -6,6 +6,6 @@ from django.contrib import admin
 from apps.contacts.models import *
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','given_name','family_name')
+    list_display = ('id', 'debug_id', 'common_name', 'given_name', 'family_name')
 
 admin.site.register(Contact, ContactAdmin)
--- smsforum/admin.py
+++ smsforum/admin.py
@@ -2,4 +2,17 @@
 # vim: ai ts=4 sts=4 et sw=4
 from django.contrib import admin
 from apps.smsforum.models import *
+from apps.contacts.models import Contact
 
+class VillageAdmin(admin.ModelAdmin):
+    def number_of_members(self, vil):
+        return str(len(vil.flatten()))
+
+    def members(self, vil):
+        return ', '.join([m.get_signature() for m in vil.flatten(klass=Contact)])
+
+    list_display = ('name', 'number_of_members', 'members')
+   
+    fields = ('name', '_childleaves')
+   
+admin.site.register(Village, VillageAdmin)
