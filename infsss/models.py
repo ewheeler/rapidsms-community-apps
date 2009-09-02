32ea9244306e1a7816a6785d8d257089a2fed5a9
Meredith <meredith@system76-netbook.(none)>
2009-09-02 14:40:38 -0400

    INFSS Model - basically a bogus class that maintains a list of RapidSMS locations

---
+++ infsss/models.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+from django.db import models
+from django.contrib.auth import models as auth_models
+from django.core.exceptions import ObjectDoesNotExist 
+from datetime import datetime, date
+from apps.locations.models import Location  
+from apps.reporters.models import Reporter
+
+#bogus
+class LocationStatus(models.Model):
+    location   = models.ForeignKey(Location)
+
+    def __unicode__(self): 
+        return "%s" % self.location.name
