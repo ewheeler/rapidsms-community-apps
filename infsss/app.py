8a57906a843468f7d54695c5ba5f752724a83303
Meredith <meredith@system76-netbook.(none)>
2009-09-02 14:36:42 -0400

    Added Views and Templates to INFSSS need to add sms response, parsing, graphing, pagination and breadcrumbs in that order

--- infsss/app.py
+++ infsss/app.py
@@ -1,77 +1,214 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
+from datetime import date, datetime
+from strings import ENGLISH as STR
 
-import re
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
 import rapidsms
-import datetime
-from models import *
+from rapidsms.message import Message
+from rapidsms.connection import Connection
+from rapidsms.parsers.keyworder import * 
 
-
-#CRAPPY WITH TOO MUCH HARD CODING - BUT WANT SOMETHING TO GET STARTED WITH
+from  models import * 
+import graph
+import utils
 
 class App(rapidsms.app.App):
-    #Upgrade this to keyword parser
-    #NEW = re.compiler(r"^(new|add|exit|help)\ (.*?)\ ?(.*?)\ ?(.*?)\ ?(.*?)\ ?(.*?)\ ?
-    RESP = ["new reading: n GMCid ChildID Weight(0.0) Height(0.0) MUAC(0.00) Oedema(Y/N) Diarrhoea(Y/N)",\
-            "add child: a Sex(M/F) AgeInMonths GMCid Weight(0.0) Height(0.0) MUAC(0.00) Oedema(Y/N) Diarrhoea(Y/N)",\
-            "h ([N]ew reading,[A]dd child)- enter h n or h a",\
-            "Please resend your message, type h for more help",\
-            "Unable to find GMCId - please fix the Id and resend the message. type: h n -for more help"\
-            "Please add a child: a Sex(M/F) AgeInMonths"\
-            "Thank you for the excellent data reading."\
-            "Thank you for the excellent data reading - The Child Id is %s"]
+    msgs = {"help":""}
+
+    # lets use the Keyworder parser!
+    kw = Keyworder()
+
+    def parse(self, message):
+        self.handled = False 
 
 
-
-
-    def __find_patient(self, text):
+    def handle(self, message):
         try:
-            return Patient.objects.get(id=text)
+            if hasattr(self, "kw"):
+                try:
+                    # attempt to match tokens in this message
+                    # using the keyworder parser
+                    func, captures = self.kw.match(self, message.text)
+                    func(self, message, *captures)
+                    # short-circuit handler calls because 
+                    # we are responding to this message
+                    return self.handled 
+                except Exception, e:
+                    # TODO only except NoneType error
+                    # nothing was found, use default handler
+                    self.incoming_entry(message)
+                    return self.handled 
+            else:
+                self.debug("App does not instantiate Keyworder as 'kw'")
+        except Exception, e:
+	    # TODO maybe don't log here bc any message not meant
+	    # for this app will log this error
+	    #
+            # self.error(e) 
+	    pass
 
-        except Patient.DoesNotExist:
+
+    def outgoing(self, message):
+        pass 
+
+    # HELP
+    @kw("help")
+    def help(self, message):
+        message.respond([
+            "subscribe",
+            "unsubscribe"])
+    
+    
+    def __get(self, model, **kwargs):
+        try:
+            # attempt to fetch the object
+            return model.objects.get(**kwargs)
+        
+        # no objects or multiple objects found (in the latter case,
+        # something is probably broken, so perhaps we should warn)
+        except (ObjectDoesNotExist, MultipleObjectsReturned):
             return None
 
-    #can i inherit this?
-    def __find_gmc(self, text):
-        try:
-            #we only want gmc sites
-            return Location.objects.get(id=text,type=4)
+    # SUBSCRIBE ---------------------------------------------------------------
+    
+    kw.prefix = ["subscribe", "join"]
 
-        except Location.DoesNotExist:
-            return None
+    @kw.blank()
+    @kw("(whatever)")
+    def subscribe(self, message, blah=None):
+        r, created = p.Respondant.subscribe(str(message.connection))
+        
+        # acknowledge with an appropriate message
+        if created: message.respond(STR["subscribe"])
+        else: message.respond(STR["resubscribe"])
+    
+    
+    # UNSUBSCRIBE -------------------------------------------------------------
+    
+    kw.prefix = ["unsubscribe", "leave", "stop", "exit"]
 
-    def parse(self, msg):
-      
-        #Use Keyword parser - I dont need regex for this test
+    @kw.blank()
+    @kw("(whatever)")
+    def unsubscribe(self, message, blah=None):
+        r, created = p.Respondant.unsubscribe(message.connection)
+        message.respond(STR["unsubscribe"])
+	self.handled = True
 
-        #sort of crap bu lets get something out teh door
-        #not so into index paradigm
-        #creates will not work
-        param = ()
+    # SUBMIT AN ANSWER --------------------------------------------------------
 
-        respindex = 3 
-        if msg is not None: 
+    def incoming_entry(self, message):
+        # make a poll.Message out of the rapidsms.models.Message
+        # because utils.parse_message expects a poll.Message
+        mess = p.Message.objects.create(is_outgoing=False,\
+            connection=str(message.connection), text=message.text)
 
-            v = msg.split(" ") #tokenize because I am bogus - will fix w/ field feedback
-            if ("h" in v[0] ):
-                respindex = int("a" in msg)
-                #respindex = "h" in v[0] * int("a" in msg) too insane perhaps
-            else: 
-                # is first token a number or letter  set i
-                i = 0
-                        
-                if ("n" in v[0]) or (len(v) == 7): 
-                    patient = self.__find_patient_id(v[i+1]) 
-                elif ("a" in v[0]):
-                        patient =   Patient(gender=v[i],system=1, dob=datetime.datetime.now()+datetime.timedelta(v[i+1]*30)
-                        patient.save()
-                gmc = self.__find_gmc(v[i])
-                if patient is None : respindex = 5
-                if gmc is None : respindex = 4
-                infsss = INFSS(patient=patient, location=gmc,height=v[i+1],weight=v[i+2],muac=v[i+3],oedema=v[i+4],diarrea=v[i+5])
-                infsss.save()
-                param =  (patient.id)
-             
-            msg.respond = RESP[respindex] % param
+        # ensure that the caller is subscribed
+        r, created = p.Respondant.subscribe(str(message.connection))
+        # if no question is currently running, then
+        # we can effectively ignore the incoming sms,
+        # but should notify the caller anyway
+        ques = p.Question.current()
+        if ques is None: 
+	    message.respond(STR["no_question"])
+	    self.handled = True
+        
+        # try to parse the message
+        # pass along the rapidsms.models.Message.backend with the
+        # poll.Message object so that parse_message can check that
+        # the respondant is subscribed
+        parsed = utils.parse_message(mess, ques)
 
+        # send an appropriate response to the caller
+        if parsed:  
+            graph.graph_entries(ques)
+            message.respond(STR["thanks"])
+	    self.handled = True
+
+        else:       
+	    message.respond(STR["thanks_unparseable"])
+	    self.handled = True
+
+	# BROADCAST FUNCTIONS ----------------------------------------------------------
+	
+	def broadcast_question(question):
+
+		# gather active respondants
+		respondants = p.Respondant.objects.filter(is_active=True)
+		sending = 0
+
+		# message to be blasted
+		broadcast = question.text
+
+		# unless this is a free text question,
+		# add the answer choices to the broadcast message
+		if question.type != 'F':
+			answers = p.Answer.objects.filter(question=question.pk)
+			for a in answers:
+				broadcast = broadcast + '\n ' + a.choice + ' - ' + a.text
+
+		# blast the broadcast message to our active respondants
+		# and increment the counter
+		for r in respondants:
+			r.connection.backend.send(r.connection.identity, broadcast)
+			sending += 1
+			self.info('[broadcaster] Blasted to %d of %d numbers...' % (sending, len(respondants)))
+
+		# save number broadcasted to db
+		question.sent_to = sending
+		question.save()
+
+		return '[broadcaster] Blasted %s to %d numbers with %d failures' % \
+				(broadcast, sending, (len(respondants) - sending))
+
+
+	def broadcaster(seconds=60, wake_hour=8, sleep_hour=21):
+		self.info("Starting Broadcaster...", "init")
+		while True:
+			
+			# only broadcast while people are awake. otherwise, we'll be sending
+			# people messages at 12:01AM, which probably won't go down so well
+			hour = time.localtime()[3]
+			if wake_hour < hour < sleep_hour:
+				
+				# if the current question has not been sent,
+				# broadcaster will broadcast it
+				q = p.Question.current()
+				if q:
+				
+					# if this question hasn't been 'sent_to' anyone yet,
+					# we can assume that it should be broadcast now
+					if not q.sent_to:
+						self.info("Broadcasting new question")
+						broadcast_question(q)
+					
+					# already sent, so we have nothing to do
+					# i don't think we really need to log this...
+					#else: app.log("Current question was already broadcast")
+					else: pass
+				
+				# when in production, there should probably ALWAYS
+				# be an active question; otherwise, anyone texting
+				# in will receive an error -- so highlight this
+				# as a warning in the screen log
+				else: self.info("No current question", "warn")
+			
+			# we are outside of waking hours, so do nothing
+			else:
+				self.info("Broadcast disabled from %d:00 to %d:00" %\
+						(sleep_hour, wake_hour))
+			
+			# wait until it's time
+			# to check again (60s)
+			time.sleep(seconds)
+
+	# BROADCAST THREAD -------------------------------------------------------------
+
+	self.info("[broadcaster] Starting up...")
+
+	# interval to check for broadcasting (in seconds)
+	broadcast_interval = 30
+	# start a thread for broadcasting
+	thread.start_new_thread(broadcaster, (broadcast_interval,))
--- infsss/urls.py
+++ infsss/urls.py
@@ -3,6 +3,8 @@
 
 
 from django.conf.urls.defaults import *
+from apps.docmanager.models import *
+
 import views as v
 
 
@@ -10,10 +12,40 @@ urlpatterns = patterns('',
     
     # mini dashboard for this app
     url(r'^infsss$',
-        v.dashboard,
-        name="all_data"),
+        v.dashboard),
+    
+    url(r'^infsss/(?P<location>Country|Region|District|GMC)$',
+        v.bylocation),
+    
+    url(r'^infsss/(?P<type>Child|HSA)$',
+        v.people),
+    
+    url(r'^infsss/(?P<type>Child)/(?P<id>\d+)$',
+        v.bychildid),
+    
+    url(r'^infsss/(?P<type>HSA)/(?P<id>\d+)$',
+        v.byhsaid),
 
+    url(r'^infsss/region/(?P<id>\d+)$',
+        v.bylocationid),
+    
+
+    url(r'^infsss/(?P<date1>\d+)-(?P<date2>\d+)$',
+        v.data_date_filter),
+    
+    url(r'^infsss/date$',
+        v.datefilter),
+   
+   url(r'^infsss/(?P<id>\d+)/download$',
+        v.download_file,
+        name='download-file'),
+
+    url(r'^infsss/(?P<type>Nutrition|Wasting|Stunting|Children|HSAs|Location)/xls$',
+        v.export_xls,
+        name="export-xls"),
+
+    #stick in docmanager view
+#    url(r'^infsss/(?P<id>\d+)/download$',
+#        'django.views.static.serve',{'document_root':'/'}), #File.objects.get(id=1).file.path}),
+    
 )
-
-#edit data in the admin interface 
-#import via csv - someplace too
